b4b118c6d398574d11ae6175e5f43eb2bf9199f8
jhs512 <jangka512@gmail.com>
2022-02-02 01:11:42 +0900

    사용자 관리 페이지 생성

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,12 +5,11 @@ use Illuminate\Routing\Router;
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
-    'as'            => config('admin.route.prefix') . '.',
+    'prefix' => config('admin.route.prefix'),
+    'namespace'  => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
+    'as' => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
     $router->get('/', 'HomeController@index')->name('home');
-
+    $router->resource('users', UserController::class);
 });
