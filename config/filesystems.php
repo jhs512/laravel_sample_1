5b0bd045523d797cf1934612d4672552795403b0
jhs512 <jangka512@gmail.com>
2022-02-02 10:53:37 +0900

    관리자 파일 업로드 설정

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,15 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+        ],
+
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('admin/uploads'),
+            'visibility' => 'public',
+            'url' => env('APP_URL') . '/admin/uploads',
         ],
 
         's3' => [
