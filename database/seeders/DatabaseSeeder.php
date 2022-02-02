218ff52ac76131d99abbc4467b20b070af4a2a80
jhs512 <jangka512@gmail.com>
2022-02-02 13:31:50 +0900

    AdminSeeder 추가

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Admin;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        $this->call([
+            AdminSeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
